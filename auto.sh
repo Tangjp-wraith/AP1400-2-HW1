706e87e05973d168e820a81b4520b90517ade0ca
Tangjp-wraith <3081129260@qq.com>
2022-11-22 18:08:19 +0800

    final commit

---
+++ auto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git status
+git add .
+git commit -m "final commit"
+git push origin master
--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -1,18 +1,16 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/usr/src/app/src/hw1.cpp" "/usr/src/app/build/CMakeFiles/main.dir/src/hw1.cpp.o"
-  "/usr/src/app/src/main.cpp" "/usr/src/app/build/CMakeFiles/main.dir/src/main.cpp.o"
-  "/usr/src/app/src/unit_test.cpp" "/usr/src/app/build/CMakeFiles/main.dir/src/unit_test.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tang/CSProjects/AP1400-2-HW/AP1400-2-HW1/src/hw1.cpp" "CMakeFiles/main.dir/src/hw1.cpp.o" "gcc" "CMakeFiles/main.dir/src/hw1.cpp.o.d"
+  "/home/tang/CSProjects/AP1400-2-HW/AP1400-2-HW1/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/tang/CSProjects/AP1400-2-HW/AP1400-2-HW1/src/unit_test.cpp" "CMakeFiles/main.dir/src/unit_test.cpp.o" "gcc" "CMakeFiles/main.dir/src/unit_test.cpp.o.d"
   )
 
 # Targets to which this target links.
